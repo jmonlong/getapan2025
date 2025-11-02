3e508275b960a5b4edf6baaf7aba5f94b4138d2b
Xian Hui Chang <xian-hui.chang@inrae.fr>
2025-11-02 15:16:12 +0100

    Add example graphs

--- example_graphs/index_graphs.sh
+++ example_graphs/index_graphs.sh
@@ -3,9 +3,11 @@ set -ex
 #Make the graphs with the python script
 python3 make_graphs.py
 
-for GRAPH in simple_nested_snarl inversion; do
+for GRAPH in simple_nested_snarl loopy_snarl loopy_chain aaa; do
 
     vg view -dp ${GRAPH}.hg | dot -Tsvg -o ${GRAPH}.svg
+
+    vg index -j ${GRAPH}.dist ${GRAPH}.hg
 
     vg deconstruct -a -p ref#0#0 ${GRAPH}.hg >${GRAPH}.deconstruct.vcf
 
--- example_graphs/make_graphs.py
+++ example_graphs/make_graphs.py
@@ -4,16 +4,16 @@ from bdsg.bdsg import HashGraph
 
 
 def make_simple_nested_snarl():
-    #                       5         9
-    #                     /   \      /  \
-    #            1       4 ----6    8---10
-    #          /   \   /         \ /     \
-    #        0       3  ----------7-------11
+    #                       5 
+    #                     /   \ 
+    #            1       4 ----6------- 
+    #          /   \   /         \      \
+    #        0       3  ----------7------8
     #          \   /
     #            2
     
     graph = HashGraph()
-    seqs = ["AGGAT", "GGCAC", "CTTAG", "AAGTC", "CGGTA", "G", "A", "GGTAC", "TGCGA", "TCATG", "GGGTG", "ATCAG"]
+    seqs = ["AGGAT", "GGCAC", "CTTAG", "AAGTC", "CGGTA", "ACTAC", "CGTCA", "CCAGG", "TGATG"]
     nodes = []
     for s in seqs:
         nodes.append(graph.create_handle(s))
@@ -28,16 +28,12 @@ def make_simple_nested_snarl():
     graph.create_edge(nodes[4], nodes[6]);
     graph.create_edge(nodes[5], nodes[6]);
     graph.create_edge(nodes[6], nodes[7]);
+    graph.create_edge(nodes[6], nodes[8]);
     graph.create_edge(nodes[7], nodes[8]);
-    graph.create_edge(nodes[7], nodes[11]);
-    graph.create_edge(nodes[8], nodes[9]);
-    graph.create_edge(nodes[8], nodes[10]);
-    graph.create_edge(nodes[9], nodes[10]);
-    graph.create_edge(nodes[10], nodes[11]);
     
-    path_seqs = [ ("ref#0#0", [0, 1, 3, 7, 8, 9, 10, 11]), 
-                   ("sample1#1#0#0", [0, 1, 3, 4, 5, 6, 7, 8, 10, 11]), 
-                   ("sample1#2#0#0", [0, 2, 3, 7, 11])]
+    path_seqs = [ ("ref#0#0", [0, 1, 3, 7, 8]), 
+                   ("sample1#1#0#0", [0, 1, 3, 4, 5, 6, 7, 8]), 
+                   ("sample1#2#0#0", [0, 2, 3, 4, 6, 8])]
     paths = [];
     
     for path_i in range(len(path_seqs)):
@@ -47,6 +43,115 @@ def make_simple_nested_snarl():
     
     
     graph.serialize("simple_nested_snarl.hg")
+
+
+def make_loopy_chain():
+    #                    2
+    #                 /    \
+    #             ----1      4---
+    #           /      \   /     \
+    #          /         3        \
+    #         /                    \
+    #        0----------------------5
+    
+    graph = HashGraph()
+    seqs = ["AGGAT", "GGCAC", "CTTAG", "AAGTC", "CGGTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "GTACG"]
+    nodes = []
+    for s in seqs:
+        nodes.append(graph.create_handle(s))
+    
+    graph.create_edge(nodes[0], nodes[1]);
+    graph.create_edge(nodes[0], nodes[5]);
+    graph.create_edge(nodes[1], nodes[2]);
+    graph.create_edge(nodes[1], nodes[3]);
+    graph.create_edge(nodes[2], nodes[4]);
+    graph.create_edge(nodes[3], nodes[4]);
+    graph.create_edge(nodes[4], nodes[5]);
+    
+    path_seqs = [ ("ref#0#0", [0, 1, 2, 4, 5]), 
+                   ("sample1#1#0#0", [0, 1, 2, 4, 5]), 
+                   ("sample1#2#0#0", [0, 1, 2, 4, 5]), 
+                   ("sample2#1#0#0", [0, 1, 3, 4, 5]), 
+                   ("sample2#2#0#0", [0, 5])]
+    paths = [];
+    
+    for path_i in range(len(path_seqs)):
+        paths.append(graph.create_path_handle(path_seqs[path_i][0]));
+        for node_i in path_seqs[path_i][1]: graph.append_step(paths[-1], nodes[node_i]);
+    
+    
+    graph.serialize("loopy_chain.hg")
+
+def make_loopy_snarl():
+    #                  2
+    #                /    \
+    #           0----1      4---5
+    #                 \   /     
+    #                   3        
+
+    graph = HashGraph()
+    seqs = ["TACCG", "AGGAT", "GGCAC", "CTTAGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "AAGTC", "GCAC"]
+    nodes = []
+    for s in seqs:
+        nodes.append(graph.create_handle(s))
+    
+    graph.create_edge(nodes[0], nodes[1]);
+    graph.create_edge(nodes[1], nodes[2]);
+    graph.create_edge(nodes[1], nodes[3]);
+    graph.create_edge(nodes[2], nodes[4]);
+    graph.create_edge(nodes[3], nodes[4]);
+    graph.create_edge(nodes[4], nodes[5]);
+    
+    path_seqs = [ ("ref#0#0", [0, 1, 3, 4, 5]), 
+                   ("sample1#1#0#0", [0, 1, 2, 4, 5]), 
+                   ("sample1#2#0#0", [0, 1, 3, 4, 5])]
+    paths = [];
+    
+    for path_i in range(len(path_seqs)):
+        paths.append(graph.create_path_handle(path_seqs[path_i][0]));
+        for node_i in path_seqs[path_i][1]: graph.append_step(paths[-1], nodes[node_i]);
+    
+    
+    graph.serialize("loopy_snarl.hg")
+
+
+
+def make_aaa():
+    #          -1---2---3---4---5
+    #         /                  \
+    #        0------------------  6
+    
+    graph = HashGraph()
+    seqs = ["AAAAAAAAAA", "A", "A", "A", "A", "A", "AAAAAAAAAAAAAAA"]
+    nodes = []
+    for s in seqs:
+        nodes.append(graph.create_handle(s))
+    
+    graph.create_edge(nodes[0], nodes[1]);
+    graph.create_edge(nodes[0], nodes[2]);
+    graph.create_edge(nodes[0], nodes[3]);
+    graph.create_edge(nodes[0], nodes[4]);
+    graph.create_edge(nodes[0], nodes[5]);
+    graph.create_edge(nodes[0], nodes[6]);
+    graph.create_edge(nodes[1], nodes[2]);
+    graph.create_edge(nodes[2], nodes[3]);
+    graph.create_edge(nodes[3], nodes[4]);
+    graph.create_edge(nodes[4], nodes[5]);
+    graph.create_edge(nodes[5], nodes[6]);
+    
+    path_seqs = [ ("ref#0#0", [0, 4, 5, 6]), 
+                   ("sample1#1#0#0", [0, 1, 2, 3, 4, 5, 6]), 
+                   ("sample1#2#0#0", [0, 3, 4, 5, 6])] 
+    paths = [];
+    
+    for path_i in range(len(path_seqs)):
+        paths.append(graph.create_path_handle(path_seqs[path_i][0]));
+        for node_i in path_seqs[path_i][1]: graph.append_step(paths[-1], nodes[node_i]);
+    
+    
+    graph.serialize("aaa.hg")
+
+
 
 
 def make_inversion():
@@ -91,4 +196,6 @@ def make_inversion():
     graph.serialize("inversion.hg")
 
 make_simple_nested_snarl()
-make_inversion()
+make_loopy_chain()
+make_loopy_snarl()
+make_aaa()
